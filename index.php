4223e0570b945001c28ed51fd198948726345f56
Sr-Stroncio <razukmamo@gmail.com>
2026-05-05 13:30:05 +0200

    añadiendo assets y empezando la lading page

--- index.php
+++ index.php
@@ -1,13 +1,49 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/lading_page.css">
     <title>Lading page</title>
     <base href="/">
 </head>
+
 <body>
-    <p>Miguel</p>
-    <h2>TEST DE RAMAS</h2>
+    <header>
+        <div class="div-logos">
+            <img class="GTI_logo" src="assets/logoGTI.svg" alt="GTI Logo">
+            <img class="DOA_logo" src="assets/DoA color.svg" alt="DOA Logo">
+        </div>
+
+        <nav>
+            <a href="#">Servicios</a>
+            <a href="#">Home</a>
+            <a href="#">Sobre nosotros</a>
+        </nav>
+
+        <a href="#">
+            <button>
+                Empieza ya
+            </button>
+        </a>
+    </header>
+
+    <section>
+
+    </section>
+
+    <section>
+
+    </section>
+
+    <section>
+
+    </section>
+
+    <footer>
+
+    </footer>
 </body>
+
 </html>
