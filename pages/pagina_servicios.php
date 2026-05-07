cc338e219d2acbdcfaace43dc4f33afcd23bed33
Sr-Stroncio <razukmamo@gmail.com>
2026-05-07 23:42:02 +0200

    terminando lading page sin datos

--- pages/pagina_servicios.php
+++ pages/pagina_servicios.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head class="header">
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Página de compra</title>
     <base href="/">
     <link rel="stylesheet" href="css/pagina_servicios.css">
     <link rel="stylesheet" href="css/lading_page.css">
 
 </head>
+
 <body>
     <header>
         <div class="div-logos">
-        <img class="GTI_logo" src="assets/logoGTI.svg" alt="GTI Logo">
-        <img class="DOA_logo" src="assets/DoA color.svg" alt="DOA Logo">
+            <img class="GTI_logo" src="assets/logoGTI.svg" alt="GTI Logo">
+            <img class="DOA_logo" src="assets/DoA color.svg" alt="DOA Logo">
         </div>
         <nav>
             <a href="#">Servicios</a>
@@ -24,7 +26,7 @@
 
         <!-- redireccion hacia la pagina de servicios -->
         <a class="btn-empezar" href="#">
-        Inicia sesion
+            Inicia sesion
         </a>
     </header>
     <!-- seleccionar plan -->
@@ -36,18 +38,18 @@
             <div class="caja_plan">
                 <h3>Pagina de prueba</h3>
                 <p>¡Empieza ahora con doa!
-                Prueba nuestra plataforma y experimenta de primera mano cómo simplifica la enseñanza. Accede a las funcionalidades clave y comprueba todo lo que puedes hacer antes de elegir un plan de compra. ¡Prueba DOA!</p>
+                    Prueba nuestra plataforma y experimenta de primera mano cómo simplifica la enseñanza. Accede a las funcionalidades clave y comprueba todo lo que puedes hacer antes de elegir un plan de compra. ¡Prueba DOA!</p>
                 <button type="button" class="boton-seleccionar-plan">Ir a la prueba</button>
             </div>
             <!-- caja plan estandar -->
             <div class="caja_plan">
                 <h3>Plan de estandar (5000€)</h3>
                 <p>Todo lo que necesitas para el día a día educativo, en un solo lugar.
-                Llena de funciones como:
-                -Anuncios
-                -Calendario
-                -Asignaturas con: calificaciones, tareas, recursos y examenes.
-                !Todo esto, solo en DOA.!</p>
+                    Llena de funciones como:
+                    -Anuncios
+                    -Calendario
+                    -Asignaturas con: calificaciones, tareas, recursos y examenes.
+                    !Todo esto, solo en DOA.!</p>
                 <button type="button" class="boton-seleccionar-plan">Seleccionar plan</button>
             </div>
             <!-- caja plan profesional -->
@@ -64,4 +66,5 @@
         </div>
     </section>
 </body>
+
 </html>
